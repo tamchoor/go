9beb39120a2c69cb4556d930460f192b19f2a67f
tamchoor <dementyevaanastasia@icloud.com>
2022-12-02 12:38:32 +0300

    1

--- d04/ex00/serv/go/routers.go
+++ d04/ex00/serv/go/routers.go
@@ -44,7 +44,7 @@ func NewRouter() *mux.Router {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	fmt.Fprintf(w, "Hello Worlddddd!")
 }
 
 var routes = Routes{
