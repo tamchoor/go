536fc4e3c4d2230e35967bea61c79aa9f412ea87
tamchoor <dementyevaanastasia@icloud.com>
2022-12-07 18:38:41 +0300

    day 06 ex01

--- d06/ex01/go.mod
+++ d06/ex01/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/go-pg/pg v8.0.7+incompatible // indirect
+	github.com/gorilla/context v1.1.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20190320223903-b7391e95e576 // indirect
 	mellium.im/sasl v0.3.0 // indirect
--- d06/ex01/main.go
+++ d06/ex01/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/go-pg/pg"
+	"github.com/gorilla/context"
 	// "github.com/go-pg/pg/orm"
+	"bufio"
 	"html/template"
 	"net/http"
 	"os"
-	// "github.com/go-pg/pg"
-	// "github.com/go-pg/pg/orm"
+	"strconv"
 )
 
 type Post struct {
@@ -18,26 +19,43 @@ type Post struct {
 	// AuthorEmail string
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/homePage.html")
+func adminPage(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/adminPage.html", "templates/showAll.html")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	db := DBConn()
 	var posts []Post
-
+	defer db.Close()
 	err = db.Model(&posts).Select()
 	if err != nil {
 		panic(err)
 	}
 
-	// posts[0].Content = "first"
-	// posts[1].Content = "second"
 	extra := struct {
 		Posts []Post
 	}{Posts: posts}
+	tmpl.Execute(w, extra)
+}
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/homePage.html", "templates/showAll.html")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	db := DBConn()
+	var posts []Post
+	defer db.Close()
+	err = db.Model(&posts).Select()
+	if err != nil {
+		panic(err)
+	}
+
+	extra := struct {
+		Posts []Post
+	}{Posts: posts}
 	tmpl.Execute(w, extra)
 }
 
@@ -50,25 +68,148 @@ func DBConn() (db *pg.DB) {
 	return db
 }
 
-func addContentInDB() {
+func DeleteContentInDB() {
 	db := DBConn()
 	var posts []Post
-
+	defer db.Close()
 	err := db.Model(&posts).Select()
 	if err != nil {
 		panic(err)
 	}
 
-	if posts[0].Content == "" {
-		posts[0].Content = "first"
+	if posts != nil {
+		res, err := db.Model(&posts).Delete()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("deleted", res.RowsAffected())
 	}
-	if posts[0].Content == "" {
-		posts[1].Content = "second"
+}
+
+func addContentInDB() {
+	db := DBConn()
+	post := Post{
+		Title:   "title 3",
+		Content: "third content",
+	}
+	defer db.Close()
+	err := db.Insert(&post)
+	if err != nil {
+		panic(err)
 	}
 
 }
 
+func showPage(w http.ResponseWriter, r *http.Request) {
+	nId, err := strconv.ParseInt(r.URL.Query().Get("id"), 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Fprintf(w, "nid =  %d", nId)
+
+	tmpl, err := template.ParseFiles("templates/homePage.html", "templates/showOne.html")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	db := DBConn()
+	var posts []Post
+	defer db.Close()
+	err = db.Model(&posts).Select()
+	if err != nil {
+		panic(err)
+	}
+
+	var i int
+	for i = range posts {
+		if posts[i].Id == nId {
+			break
+		}
+	}
+
+	fmt.Println("i =", i)
+	fmt.Println("posts[i].Id =", posts[i].Id)
+
+	extra := struct {
+		Id      int64
+		Title   string
+		Content string
+	}{Id: posts[i].Id, Title: posts[i].Title, Content: posts[i].Content}
+
+	tmpl.Execute(w, extra)
+}
+
+func loginPage(w http.ResponseWriter, r *http.Request) {
+	data := context.Get(r, "data")
+	t, _ := template.ParseFiles("templates/homePage.html", "templates/login.html")
+	t.Execute(w, data)
+}
+
+func loginCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("rrrrr")
+	if r.Method == "POST" {
+		login := r.FormValue("login")
+		passw := r.FormValue("passw")
+
+		file, err := os.Open("templates/admin_credentials.txt")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		fileScanner := bufio.NewScanner(file)
+		fileScanner.Scan()
+		fmt.Println(fileScanner.Text())
+		correctLog := fileScanner.Text()
+		fileScanner.Scan()
+		fmt.Println(fileScanner.Text())
+		correctPassw := fileScanner.Text()
+
+		if correctPassw == passw && correctLog == login {
+			fmt.Println("coorect")
+			http.Redirect(w, r, "/admin", 301)
+		} else {
+			fmt.Println("not coorect")
+			http.Redirect(w, r, "/", 301)
+		}
+	}
+}
+
+func newPostPage(w http.ResponseWriter, r *http.Request) {
+	data := context.Get(r, "data")
+	t, _ := template.ParseFiles("templates/homePage.html", "templates/new.html")
+	t.Execute(w, data)
+}
+
+func newPostInsert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		db := DBConn()
+		title := r.FormValue("title")
+		content := r.FormValue("content")
+		post1 := &Post{
+			Title:   title,
+			Content: content,
+		}
+		err := db.Insert(post1)
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+		http.Redirect(w, r, "/", 301)
+	}
+}
+
 func main() {
+	// DeleteContentInDB()
+	// addContentInDB()
+
+	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("./templates"))))
 	http.HandleFunc("/", homePage)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/show", showPage)
+	http.HandleFunc("/login", loginPage)
+	http.HandleFunc("/login/insert/", loginCheck)
+	http.HandleFunc("/admin", adminPage)
+	http.HandleFunc("/admin/newPost", newPostPage)
+	http.HandleFunc("/admin/newPost/insert/", newPostInsert)
+	http.ListenAndServe(":8888", nil)
 }
